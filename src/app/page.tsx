b616b87035681e370d8f990b6f74285121e94733
Nguyễn Trọng <trongnp144@gmail.com>
2024-09-25 15:44:25 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "antd";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -48,6 +50,9 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <Link href='/test'>
+          <Button type="primary">Go to TestPage </Button>
+        </Link>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
