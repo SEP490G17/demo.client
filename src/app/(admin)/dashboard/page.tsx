e4c03828dcc85d9174bc9549d705f9474c9c9dd5
ng.lonqvu <longvu131102@gmail.com>
2024-09-26 22:46:30 +0700

    base admin

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import AdminCard from "@/app/components/admin/admin.card";
+import AdminLayout from "./layout";
+
+const DashboardPage = () => {
+
+    return (
+        <div>
+            <AdminCard />
+        </div>
+    )
+}
+
+export default DashboardPage;
+
+
+
+
+
+
