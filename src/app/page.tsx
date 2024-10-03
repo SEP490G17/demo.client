1fc7271b85f86f05264ec88de1982cc6a863518a
Nguyễn Trọng <trongnp144@gmail.com>
2024-10-03 22:36:30 +0700

    mockup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import { Flex, Spin } from "antd";
+import MainContent from "./components/MainContent/MainContent";
+import SideContent from "./components/SideContent/SideContent";
+
+
 export default function Home() {
   return (
     <>
+      <Flex gap="large">
+        <MainContent />
+        <SideContent />
+      </Flex>
     </>
   );
 }
--- src/libs/stores/store.ts
+++ src/libs/stores/store.ts
@@ -1,11 +1,12 @@
 import { createContext, useContext } from "react";
+import CourtStore from "./courtStore";
 
 interface Store {
-
+  courtStore: CourtStore;
 }
 
 export const store: Store = {
-
+  courtStore: new CourtStore(),
 };
 
 export const StoreContext = createContext(store);
